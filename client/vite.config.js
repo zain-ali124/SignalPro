543d53554a8248a15b374e36c112b0a2ee6fa104
Zayn Ali <jahma72441@gmail.com>
2026-04-14 11:15:24 +0300

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base:{
+    development: '/',
+    production: '/client/'
+  },
   server: {
     port: 5173,
     proxy: {
