77b189623873a1c08a5b0a12c9eb1d4889687c2b
Zayn Ali <jahma72441@gmail.com>
2026-04-14 11:19:08 +0300

    yes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:{
-    development: '/',
-    production: '/client/'
-  },
+  // Base must be a string. Using '/' so assets are served from site root.
+  base: '/',
   server: {
     port: 5173,
     proxy: {
